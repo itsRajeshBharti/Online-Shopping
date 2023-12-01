47150e4c16f451c35a673134f7227f8d680b95b6
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-01 22:27:02 +0530

    testing server

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require("express");
+const upload = require("express-fileupload");
+const bodyparse = require("body-parser");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.SERVER_PORT;
+
+app.use(cors()); // accepting routes from all origin
+app.use(upload()); // used for accepting multipart-form-data.
+app.use("/image", express.static("public")); // used to serve static content from the server.
+app.use(bodyparse.json()); // used for accepting raw(json) data.
+app.use(bodyparse.urlencoded({ extended: true })); // used for accepting x-www-form-urlencoded.
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .json({ status: true, message: "Welcome to online shopping app." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT: ${PORT}`);
+});
