a99c2324254e978c58301bd70d526898b1ba2f00
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-01 22:36:13 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const upload = require("express-fileupload");
 const bodyparse = require("body-parser");
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
-app.use(cors()); // accepting routes from all origin
+app.use(cors()); // accepting routes from all origin.
 app.use(upload()); // used for accepting multipart-form-data.
 app.use("/image", express.static("public")); // used to serve static content from the server.
 app.use(bodyparse.json()); // used for accepting raw(json) data.
@@ -15,7 +15,7 @@ app.use(bodyparse.urlencoded({ extended: true })); // used for accepting x-www-f
 app.get("/", (req, res) => {
   return res
     .status(200)
-    .json({ status: true, message: "Welcome to online shopping app." });
+    .json({ status: true, message: "Welcome to Online Shopping App." });
 });
 
 app.listen(PORT, () => {
