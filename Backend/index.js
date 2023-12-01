837b676977557a2476cec9dfa145afa02130854b
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-01 22:41:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.SERVER_PORT;
 
 app.use(cors()); // accepting routes from all origin.
 app.use(upload()); // used for accepting multipart-form-data.
-app.use("/image", express.static("public")); // used to serve static content from the server.
+app.use("/content", express.static("public")); // used to serve static content from the server.
 app.use(bodyparse.json()); // used for accepting raw(json) data.
 app.use(bodyparse.urlencoded({ extended: true })); // used for accepting x-www-form-urlencoded.
 
